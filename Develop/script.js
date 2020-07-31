8b3832bfc056888d4ee4ee2c4583bcbab96321f1
Philip Robbins <plrobbins68@outlook.com>
2020-07-31 07:53:14 -0400

    updating the repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var passwordChar = prompt("How many characters would you like your password to contain?");
+var specialChar = confirm("Click ok to confirm special characters");
+var numChar = confirm("Click ok to confirm numeric characters");
+var lowerChar = confirm("Click ok to confirm lowercase characters");
+var upperChar = confirm("Click ok to confirm uppercase characters");
