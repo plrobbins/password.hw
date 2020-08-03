0af4603d11a28b826e124b7a0bcf759bbc8b0815
Philip Robbins <plrobbins68@outlook.com>
2020-08-03 16:28:01 -0400

    used an array to hold choices

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -22,44 +22,36 @@ function generatePassword() {
   }
 
 
-  //confirm special characters in the password
-  var specialChar = confirm("Click ok to confirm special characters");
-  //confirm numeric characters in the password
-  var numChar = confirm("Click ok to confirm numeric characters");
-  //confirm lowercase characters in the password
-  var lowerChar = confirm("Click ok to confirm lowercase characters");
-  //confirm uppercase characters in the password
-  var upperChar = confirm("Click ok to confirm uppercase characters");
-  //creating a loop
-  while (!specialChar && !numChar && !lowerChar&& !upperChar) {
-    alert("You have to choose at least one parameter");
-    var specialChar = confirm("Click ok to confirm special characters");
-    var numChar = confirm("Click ok to confirm numeric characters");
-    var lowerChar = confirm("Click ok to confirm lowercase characters");
-    var upperChar = confirm("Click ok to confirm uppercase characters");
-  }
-  //Password parameters
-  var passwordChar = []
-  if (specialChar) {
-    passwordChar = passwordChar.concat(specialCharacters);
-  }
-  if (numChar) {
-    passwordChar = passwordChar.concat(number);
-  }
-  if (lowerChar) {
-    passwordChar = passwordChar.concat(lowerCharacters);
-  }
-  if (upperChar) {
-    passwordChar = passwordChar.concat(upperCharacters);
+  var randomCharacters =[]
+  while (randomCharacters.length === 0) {
+    //alert("You have to choose at least one parameter");
+    if(confirm("Click ok to confirm special characters")){
+      randomCharacters.push(specialCharacters)
+    }
+    if(confirm("Click ok to confirm numeric characters")){
+      randomCharacters.push(number)
+    }
+    if(confirm("Click ok to confirm lowercase characters")){
+      randomCharacters.push(lowerCharacters)
+    }
+    if(confirm("Click ok to confirm uppercase characters")){
+      randomCharacters.push(upperCharacters)
+    }
+    if(randomCharacters.length === 0){
+      alert('You must select at least one character type')
+    }
   }
 
-  console.log(passwordChar);
+ 
 
   //Looping to generate the randon password
   var randomPassword = ""
 
   for (var i = 0; i < passwordLength; i++) {
-    randomPassword = randomPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+    var randomArray = Math.floor(Math.random()*randomCharacters.length)
+    var randomArrayChar = Math.floor(Math.random()*randomCharacters[randomArray].length)
+    randomPassword += randomCharacters[randomArray][randomArrayChar]
+    //randomPassword = randomPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
   }
   console.log(randomPassword);
   return randomPassword
